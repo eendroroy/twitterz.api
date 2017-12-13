68ac6a29cc8c4e3a1c933a123bf0e34a3985d8ef
indrajit <eendroroy@gmail.com>
2017-12-13 15:01:23 +0600

    initial commit

---
+++ src/main/groovy/com/github/eendroroy/twitterz/api/TwitterZapiApplication.groovy
@@ -0,0 +1,12 @@
+package com.github.eendroroy.twitterz.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TwitterZapiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TwitterZapiApplication, args
+	}
+}
