ec3fa90b4c0efe2ef0faf899a097f927ef09a20f
indrajit <eendroroy@gmail.com>
2017-12-24 17:37:56 +0600

    update tests

--- src/test/groovy/com/github/eendroroy/twitterz/api/service/UserServiceTest.groovy
+++ src/test/groovy/com/github/eendroroy/twitterz/api/service/UserServiceTest.groovy
@@ -2,6 +2,7 @@ package com.github.eendroroy.twitterz.api.service
 
 import com.github.eendroroy.twitterz.api.entity.User
 import com.github.eendroroy.twitterz.api.repository.UserRepository
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,16 +26,21 @@ class UserServiceTest {
         assertThat(userService.allUsers().size()).isEqualTo(0)
     }
 
+    @After
+    void tearDown(){
+        userRepository.deleteAll()
+    }
+
     @Test
     void createUser() throws Exception {
         User user1 = new User()
         user1.userName = 'user1'
-        user1.password = 'password'
+        user1.password = 'password1'
         user1.email = 'user1@example.com'
 
         User user2 = new User()
         user2.userName = 'user2'
-        user2.password = 'password'
+        user2.password = 'password2'
         user2.email = 'user2@example.com'
 
         assertThat(userService.saveUser(user1)).isNotEqualTo(null)
@@ -42,34 +48,26 @@ class UserServiceTest {
 
         assertThat(userService.saveUser(user2)).isNotEqualTo(null)
         assertThat(userService.allUsers().size()).isEqualTo(2)
-
-        userRepository.deleteAll()
-        assertThat(userService.allUsers().size()).isEqualTo(0)
     }
 
     @Test
     void findUserByEmail() throws Exception {
-        User user1 = new User()
-        user1.userName = 'user1'
-        user1.password = 'password'
-        user1.email = 'user1@example.com'
+        User user3 = new User()
+        user3.userName = 'user3'
+        user3.password = 'password3'
+        user3.email = 'user3@example.com'
 
-        User user2 = new User()
-        user2.userName = 'user2'
-        user2.password = 'password'
-        user2.email = 'user2@example.com'
+        User user4 = new User()
+        user4.userName = 'user4'
+        user4.password = 'password4'
+        user4.email = 'user4@example.com'
 
-        assertThat(userService.saveUser(user1)).isNotEqualTo(null)
-        assertThat(userService.saveUser(user2)).isNotEqualTo(null)
+        assertThat(userService.saveUser(user3)).isNotEqualTo(null)
+        assertThat(userService.saveUser(user4)).isNotEqualTo(null)
         assertThat(userService.allUsers().size()).isEqualTo(2)
-
-        assertThat(userService.findUserByEmail('user1@example.com')).isEqualToComparingFieldByField(user1)
-        assertThat(userService.findUserByEmail('user2@example.com')).isEqualToComparingFieldByField(user2)
-
-        assertThat(userService.findUserByEmail('user1@example.com')).isNotEqualTo(user2)
-        assertThat(userService.findUserByEmail('user2@example.com')).isNotEqualTo(user1)
-
-        userRepository.deleteAll()
-        assertThat(userService.allUsers().size()).isEqualTo(0)
+        assertThat(userService.findUserByEmail('user3@example.com')).isEqualToComparingFieldByField(user3)
+        assertThat(userService.findUserByEmail('user3@example.com')).isNotEqualTo(user4)
+        assertThat(userService.findUserByEmail('user4@example.com')).isEqualToComparingFieldByField(user4)
+        assertThat(userService.findUserByEmail('user4@example.com')).isNotEqualTo(user3)
     }
 }
