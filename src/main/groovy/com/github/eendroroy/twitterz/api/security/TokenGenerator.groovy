b8a9418f794b409ee468202d3ead5c5b4feb0956
indrajit <eendroroy@gmail.com>
2017-12-25 02:32:54 +0600

    update token generator

--- src/main/groovy/com/github/eendroroy/twitterz/api/security/TokenGenerator.groovy
+++ src/main/groovy/com/github/eendroroy/twitterz/api/security/TokenGenerator.groovy
@@ -1,13 +1,19 @@
 package com.github.eendroroy.twitterz.api.security
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.impl.crypto.MacProvider
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
-import java.security.SecureRandom
+import java.security.Key
 
 /**
+ *
  * @author indrajit
  */
 
+@Configuration
 class TokenGenerator {
 
     @Bean('generator')
@@ -17,10 +23,8 @@ class TokenGenerator {
 
     class Generator {
         String token() {
-            SecureRandom sr = new SecureRandom()
-            byte[] bytes = new byte[16]
-            sr.nextBytes(bytes)
-            new String(bytes)
+            Key key = MacProvider.generateKey()
+            Jwts.builder().setSubject('twitter.z.api').signWith(SignatureAlgorithm.HS512, key).compact()
         }
     }
 
