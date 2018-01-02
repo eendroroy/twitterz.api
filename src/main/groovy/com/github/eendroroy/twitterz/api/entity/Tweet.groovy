9c601693d46a35419f07a0033beade7f9a7602d0
indrajit <eendroroy@gmail.com>
2018-01-02 16:14:12 +0600

    tweet model

---
+++ src/main/groovy/com/github/eendroroy/twitterz/api/entity/Tweet.groovy
@@ -0,0 +1,38 @@
+package com.github.eendroroy.twitterz.api.entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
+import javax.validation.constraints.NotEmpty
+
+/**
+ *
+ * @author indrajit
+ */
+
+@Entity
+@Table(name = 'tweets')
+class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator='tweet_id_seq')
+    @SequenceGenerator(name='tweet_id_seq', sequenceName='tweets_tweet_id_seq', allocationSize=1)
+    @Column(name = 'tweet_id')
+    long id
+
+    @Column(name = 'body', unique = false)
+    @NotEmpty(message = '*Please provide a tweet')
+    String body
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name='user_id', nullable=false)
+    User user
+}
