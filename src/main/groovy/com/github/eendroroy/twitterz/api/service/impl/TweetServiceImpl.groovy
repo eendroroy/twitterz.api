ddc51d34029317d8290e6bef738e03fb98c3c02e
indrajit <eendroroy@gmail.com>
2018-01-02 16:18:25 +0600

    fix codenarc

--- src/main/groovy/com/github/eendroroy/twitterz/api/service/impl/TweetServiceImpl.groovy
+++ src/main/groovy/com/github/eendroroy/twitterz/api/service/impl/TweetServiceImpl.groovy
@@ -6,6 +6,7 @@ import com.github.eendroroy.twitterz.api.service.TweetService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+
 /**
  *
  * @author indrajit
