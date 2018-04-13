eb1c4858282ba356ababe627c1c804872242d9f1
indrajit <eendroroy@gmail.com>
2018-04-14 01:26:46 +0600

    added user controller test

---
+++ src/test/kotlin/com/github/eendroroy/twitterz/api/controller/UserControllerTest.kt
@@ -0,0 +1,44 @@
+package com.github.eendroroy.twitterz.api.controller
+
+import com.github.eendroroy.twitterz.api.resource.UserResource
+import com.github.eendroroy.twitterz.api.test.helper.BaseTester
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserControllerTest : BaseTester(){
+    @Autowired
+    private val template: TestRestTemplate? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setup() {
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUserShouldBeCreated() {
+        val id = (0..1000).shuffled().last()
+        val user = getHttpEntity("{\"user\": {" +
+                "\"userName\": \"user$id\"," +
+                "\"fullName\": \"user$id\"," +
+                "\"email\": \"u$id@e.c\"," +
+                "\"password\": \"password\"" +
+                "}}")
+        val resultAsset = template!!.postForEntity("/users/register", user, UserResource::class.java)
+        Assert.assertNotNull(resultAsset.body!!.user)
+        Assert.assertNotNull(resultAsset.body!!.user!!.id)
+        Assert.assertEquals(resultAsset.body!!.user!!.userName, "user$id")
+        Assert.assertEquals(resultAsset.body!!.user!!.fullName, "user$id")
+        Assert.assertEquals(resultAsset.body!!.user!!.active, 1)
+    }
+}
