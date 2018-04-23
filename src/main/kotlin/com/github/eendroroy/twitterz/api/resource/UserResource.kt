61febf9b5ecc3016f8a35177f54debf56db262c7
indrajit <eendroroy@gmail.com>
2018-04-23 23:59:44 +0600

    added register link to user resource

--- src/main/kotlin/com/github/eendroroy/twitterz/api/resource/UserResource.kt
+++ src/main/kotlin/com/github/eendroroy/twitterz/api/resource/UserResource.kt
@@ -1,8 +1,11 @@
 package com.github.eendroroy.twitterz.api.resource
 
+import com.github.eendroroy.twitterz.api.controller.UserController
 import com.github.eendroroy.twitterz.api.entity.User
 import org.springframework.hateoas.ResourceSupport
 import org.springframework.hateoas.core.Relation
+import org.springframework.hateoas.mvc.ControllerLinkBuilder
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
 
 /**
  *
@@ -18,6 +21,7 @@ class UserResource() : ResourceSupport() {
     constructor(user: User?, message: String?) : this() {
         this.user = user
         this.message = message
+        this.add(ControllerLinkBuilder.linkTo(methodOn(UserController::class.java).register(this)).withRel("register"))
     }
 
     constructor(user: User?) : this(user, null)
